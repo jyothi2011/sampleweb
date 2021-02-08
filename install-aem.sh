c9b0b374151f576707e83d5bc0e6cafb448c9742
Jyothilakshmi Chalyaanantharaman <M8080883@mindtree.com>
2021-02-08 17:08:15 +0530

    removed one file

--- install-aem.sh
+++ install-aem.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+#sudo su -
 sudo update yum 
-sudo rpm -ivh jdk-8uversion-linux-x64.rpm
+sudo  wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm
+sudo rpm -ivh jdk-8u131-linux-x64.rpm
 sudo alternatives --install /usr/bin/java java /usr/java/jdk1.8.0_version/bin/java 200000
 export PATH="$PATH:/usr/java/latest/bin"
 set JAVA_HOME=/usr/java/latest
