64373290537915fee72d2e36d3f76579067c7003
Jyothilakshmi Chalyaanantharaman <M8080883@mindtree.com>
2021-02-08 16:34:34 +0530

    removed one file

--- install-aem.bash
+++
@@ -1,6 +0,0 @@
-sudo update yum 
-sudo rpm -ivh jdk-8uversion-linux-x64.rpm
-sudo alternatives --install /usr/bin/java java /usr/java/jdk1.8.0_version/bin/java 200000
-export PATH="$PATH:/usr/java/latest/bin"
-set JAVA_HOME=/usr/java/latest
-java -version
