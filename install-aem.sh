edc70e4846b9abe5c5da77ef1f671fc98eb4703b
Jyothilakshmi Chalyaanantharaman <M8080883@mindtree.com>
2021-02-09 17:30:01 +0530

    updated new file

--- install-aem.sh
+++ install-aem.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #echo -n "" > /tmp/text.txt
-sudo yum update 
+sudo yum update
 #sudo  wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm
 #sudo rpm -ivh jdk-8u131-linux-x64.rpm
 #sudo alternatives --install /usr/bin/java java /usr/java/jdk1.8.0_version/bin/java 200000
